f80defd0074f67f1ffe4eb7576b77583828eb827
alizamir25 <ali0786zamir@gmail.com>
2024-08-27 15:54:31 +0100

    Patch

--- app.js
+++ app.js
@@ -26,11 +26,11 @@ var getBestMove=function(game){
     var bestMove=calculateBestMove(game);
     return bestMove;
 };
-var renderMoveHistory = function (moves) {
-    var historyElement = $('#move-history').empty();
+var renderMoveHistory=function(moves){
+    var historyElement=$('#move-history').empty();
     historyElement.empty();
-    for (var i = 0; i < moves.length; i = i + 2) {
-        historyElement.append('<span>' + moves[i] + ' ' + ( moves[i + 1] ? moves[i + 1] : ' ') + '</span><br>')
+    for (var i=0;i<moves.length;i=i+2){
+        historyElement.append('<span>'+moves[i]+''+(moves[i+1]?moves[i+1]:' ')+'</span><br>')
     }
     historyElement.scrollTop(historyElement[0].scrollHeight);
 };
