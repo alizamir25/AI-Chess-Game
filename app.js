6ec0b751a21e86eb913ffea0760d69eda0e8e594
alizamir25 <ali0786zamir@gmail.com>
2024-08-27 15:52:44 +0100

    Patch

--- app.js
+++ app.js
@@ -1,25 +1,16 @@
 var board,
     game = new Chess();
-
-/*AI*/
-
 var calculateBestMove =function(game) {
-
     var newGameMoves = game.ugly_moves();
-
     return newGameMoves[Math.floor(Math.random() * newGameMoves.length)];
 
 };
-
-/* board visualization and games state handling*/
-
 var onDragStart = function (source, piece, position, orientation) {
     if (game.in_checkmate() === true || game.in_draw() === true ||
         piece.search(/^b/) !== -1) {
         return false;
     }
 };
-
 var makeBestMove = function () {
     var bestMove = getBestMove(game);
     game.ugly_move(bestMove);
@@ -29,7 +20,6 @@ var makeBestMove = function () {
         alert('Game over');
     }
 };
-
 var getBestMove = function (game) {
     if (game.game_over()) {
         alert('Game over');
@@ -37,7 +27,6 @@ var getBestMove = function (game) {
     var bestMove = calculateBestMove(game);
     return bestMove;
 };
-
 var renderMoveHistory = function (moves) {
     var historyElement = $('#move-history').empty();
     historyElement.empty();
@@ -45,64 +34,48 @@ var renderMoveHistory = function (moves) {
         historyElement.append('<span>' + moves[i] + ' ' + ( moves[i + 1] ? moves[i + 1] : ' ') + '</span><br>')
     }
     historyElement.scrollTop(historyElement[0].scrollHeight);
-
 };
-
 var onDrop = function (source, target) {
-
     var move = game.move({
         from: source,
         to: target,
         promotion: 'q'
     });
-
     removeGreySquares();
     if (move === null) {
         return 'snapback';
     }
-
     renderMoveHistory(game.history());
     window.setTimeout(makeBestMove, 250);
 };
-
 var onSnapEnd = function () {
     board.position(game.fen());
 };
-
 var onMouseoverSquare = function(square, piece) {
     var moves = game.moves({
         square: square,
         verbose: true
     });
-
     if (moves.length === 0) return;
-
     greySquare(square);
-
     for (var i = 0; i < moves.length; i++) {
         greySquare(moves[i].to);
     }
 };
-
 var onMouseoutSquare = function(square, piece) {
     removeGreySquares();
 };
-
 var removeGreySquares = function() {
     $('#board .square-55d63').css('background', '');
 };
-
 var greySquare = function(square) {
     var squareEl = $('#board .square-' + square);
-
     var background = '#a9a9a9';
     if (squareEl.hasClass('black-3c85d') === true) {
         background = '#696969';
     }
-
     squareEl.css('background', background);
 };
-
 var cfg = {
     draggable: true,
     position: 'start',
